457a4d782f1f5ff4251682250ea562f3fb66a82a
sanmquin <128648525+sanmquin@users.noreply.github.com>
2026-07-05 16:39:19 -0600

    Delete library/cloneGrid.ts

--- library/cloneGrid.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Performs a deep clone of a 2D array of numbers.
- * 
- * @param {number[][]} grid - The input 2D array to be cloned.
- * @returns {number[][]} A new 2D array containing the same values as the original.
- */
-export function cloneGrid(grid: number[][]): number[][] {
-  return grid.map((row: number[]) => [...row]);
-}
